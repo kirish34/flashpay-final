2898f3fc97edabc3d8fbf7e49109f0d152c5db11
Ayub Kinyua <ayub@skyyalla.com>
2025-06-02 22:16:19 +0300

    Switched to Firebase ENV setup

--- firebase.js
+++ firebase.js
@@ -1,10 +1,13 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FB_PROJECT_ID,
     privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FB_CLIENT_EMAIL
+    clientEmail: process.env.FB_CLIENT_EMAIL,
+    clientId: process.env.FB_CLIENT_ID,
+    privateKeyId: process.env.FB_PRIVATE_KEY_ID,
+    clientX509CertUrl: process.env.FB_CLIENT_CERT
   })
 });
 
