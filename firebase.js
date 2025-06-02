cf62e4df429173a9ce2586aada90a54434a956d6
Ayub Kinyua <ayub@skyyalla.com>
2025-06-02 21:58:20 +0300

    Switch to Firebase using env vars for Render

--- firebase.js
+++ firebase.js
@@ -1,8 +1,11 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-key.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FB_PROJECT_ID,
+    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FB_CLIENT_EMAIL
+  })
 });
 
 const db = admin.firestore();
