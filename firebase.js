c46aaf5af3d053a5a12f0e1db51f4b527da5a554
Ayub Kinyua <ayub@skyyalla.com>
2025-06-02 22:48:44 +0300

    Fix Firebase env var check

--- firebase.js
+++ firebase.js
@@ -1,14 +1,16 @@
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
+
+const serviceAccount = {
+  projectId: process.env.FB_PROJECT_ID,
+  privateKeyId: process.env.FB_PRIVATE_KEY_ID,
+  privateKey: (process.env.FB_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+  clientEmail: process.env.FB_CLIENT_EMAIL,
+  clientId: process.env.FB_CLIENT_ID,
+  client_x509_cert_url: process.env.FB_CLIENT_CERT
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FB_PROJECT_ID,
-    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FB_CLIENT_EMAIL,
-    clientId: process.env.FB_CLIENT_ID,
-    privateKeyId: process.env.FB_PRIVATE_KEY_ID,
-    clientX509CertUrl: process.env.FB_CLIENT_CERT
-  })
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
